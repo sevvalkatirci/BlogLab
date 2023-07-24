93c1402ccbf09c764f9f9b729d9964ba2217129a
sevvalkatirci <sevvalkatirci99@gmail.com>
2023-07-24 20:24:49 +0300

    Repositories added

---
+++ BlogLab.Repository/IBlogRepository.cs
@@ -0,0 +1,23 @@
+﻿using BlogLab.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Repository
+{
+    public interface IBlogRepository
+    {
+        public Task<Blog> UpsertAsync(BlogCreate blogCreate, int applicationUserId);
+        public Task<PageResults<Blog>> GetAllAsync(BlogPaging blogPaging);
+
+        public Task<Blog> GetAsync(int blogId);
+
+        public Task<List<Blog>> GetAllByuserIdAsync(int applicationUserId);
+        public Task<List<Blog>> GetAllFamousAsync();
+        public Task<int> DeleteAsync(int blogId);
+
+
+    }
+}
