3484297fd7d8ae287b90b1b53e5477713d5656f0
sevvalkatirci <sevvalkatirci99@gmail.com>
2023-07-26 20:03:29 +0300

    Services project added

---
+++ BlogLab.Services/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using BlogLab.Models.Account;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Services
+{
+    public class TokenService:ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer; 
+        public TokenService(IConfiguration config) 
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            _issuer = config["Jwt:Issuer"];
+        }
+
+        public string CreateToken(ApplicationUserIdentity user)
+        {
+            var claims=new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames)
+            }
+        }
+    }
+}
